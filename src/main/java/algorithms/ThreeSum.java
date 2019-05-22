659e8feecf508ed2a03ec31082b1ac2443eff75d
gonzalez.paez.sergio@gmail.com <gonzalez.paez.sergio@gmail.com>
2019-05-22 00:31:21 -0700

    testing 3 sum

--- src/main/java/algorithms/ThreeSum.java
+++ src/main/java/algorithms/ThreeSum.java
@@ -1,7 +1,10 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class ThreeSum {
 
+  BinarySearch binarySearch = new BinarySearch();
   // count number of triples that sum 0
   public int count(int[] a) {
     int result = 0;
@@ -11,6 +14,35 @@ public class ThreeSum {
           if (0 == a[i] + a[j] + a[k]) {
             result++;
           }
+        }
+      }
+    }
+    return result;
+  }
+
+  public int countEnhanced(int[] a) {
+    Arrays.sort(a);
+    int result = 0;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = i + 1; j < a.length; j++) {
+        int r =  binarySearch.rank(-(a[i] + a[j]),a);
+        if (-1<r &&j<r) {
+          result++;
+        }
+      }
+    }
+    return result;
+  }
+
+  public int countEnhancedWithSortedArray(int[] a) {
+    System.out.println(a);
+    Arrays.sort(a);
+    System.out.println(a);
+    int result = 0;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = i + 1; j < a.length; j++) {
+        if (-1 != binarySearch.rank(-(a[i] + a[j]), a)) {
+          result++;
         }
       }
     }
--- src/test/java/algorithms/ThreeSumTest.java
+++ src/test/java/algorithms/ThreeSumTest.java
@@ -2,57 +2,95 @@ package algorithms;
 
 import edu.princeton.cs.algs4.In;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
 public class ThreeSumTest {
-
-  Stopwatch timer;
-  ThreeSum threeSum;
-
-  @Before
-  public void setup() {
-    timer = new Stopwatch();
-    threeSum = new ThreeSum();
-  }
 
   @Test
   public void test1Kints() {
-    int[] a = In.readInts("resources/algorithms/1Kints.txt");
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/1Kints.txt");
+    ThreeSum threeSum = new ThreeSum();
     int result = threeSum.count(a);
     double time = timer.elapsedTime();
-    System.out.println("number of 3-ples:" + result + ", time:" + time);
+    System.out.println("test1Kints:" + result + ", time:" + time);
+    Assert.assertEquals(70, result);
+  }
+
+  @Test
+  public void test1KintsEnhanced() {
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/1Kints.txt");
+    ThreeSum threeSum = new ThreeSum();
+    int result = threeSum.countEnhanced(a);
+    double time = timer.elapsedTime();
+    System.out.println("test1KintsEnhanced:" + result + ", time:" + time);
     Assert.assertEquals(70, result);
   }
 
   @Test
   public void test2Kints() {
-    int[] a = In.readInts("resources/algorithms/2Kints.txt");
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/2Kints.txt");
+    ThreeSum threeSum = new ThreeSum();
     int result = threeSum.count(a);
     double time = timer.elapsedTime();
-    System.out.println("number of 3-ples:" + result + ", time:" + time);
+    System.out.println("test2Kints:" + result + ", time:" + time);
+    Assert.assertEquals(528, result);
+  }
+
+  @Test
+  public void test2KintsEnchanced() {
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/2Kints.txt");
+    ThreeSum threeSum = new ThreeSum();
+    int result = threeSum.countEnhanced(a);
+    double time = timer.elapsedTime();
+    System.out.println("test2KintsEnchanced:" + result + ", time:" + time);
     Assert.assertEquals(528, result);
   }
 
   @Test
   public void test4Kints() {
-    int[] a = In.readInts("resources/algorithms/4Kints.txt");
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/4Kints.txt");
     ThreeSum threeSum = new ThreeSum();
     int result = threeSum.count(a);
     double time = timer.elapsedTime();
-    System.out.println("number of 3-ples:" + result + ", time:" + time);
+    System.out.println("test4Kints:" + result + ", time:" + time);
     Assert.assertEquals(4039, result);
   }
 
   @Test
-  public void test8Kints() {
-    int[] a = In.readInts("resources/algorithms/8Kints.txt");
+  public void test4KintsEnhanced() {
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/4Kints.txt");
     ThreeSum threeSum = new ThreeSum();
-    int result = threeSum.count(a);
+    int result = threeSum.countEnhanced(a);
     double time = timer.elapsedTime();
-    System.out.println("number of 3-ples:" + result + ", time:" + time);
-    Assert.assertEquals(32074, result);
+    System.out.println("test4KintsEnhanced:" + result + ", time:" + time);
+    Assert.assertEquals(4039, result);
+  }
+
+  @Test
+  public void test4KintsEnhancedWithSortedArray() {
+    Stopwatch timer = new Stopwatch();
+    int[] a = In.readInts("src/main/resources/algorithms/4Kints.txt");
+    ThreeSum threeSum = new ThreeSum();
+    int result = threeSum.countEnhancedWithSortedArray(a);
+    double time = timer.elapsedTime();
+    System.out.println("test4KintsEnhancedWithSortedArray:" + result + ", time:" + time);
+    Assert.assertEquals(12121, result);
+  }
+
+  @Test
+  public void test10intsEnhancedWithSortedArray() {
+    Stopwatch timer = new Stopwatch();
+    int[] a = new int[] {5, 8, 3, 1, 3, 8, 8, 9, 0};
+    ThreeSum threeSum = new ThreeSum();
+    int result = threeSum.countEnhancedWithSortedArray(a);
+    double time = timer.elapsedTime();
+    System.out.println("test4KintsEnhancedWithSortedArray:" + result + ", time:" + time);
+    Assert.assertEquals(0, result);
   }
 }
